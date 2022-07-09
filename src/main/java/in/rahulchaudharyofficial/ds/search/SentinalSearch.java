bce144eefed77788081a190318fa9c4774ddd87e
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2022-07-10 00:40:41 +0530

    more search changes added

---
+++ src/main/java/in/rahulchaudharyofficial/ds/search/SentinalSearch.java
@@ -0,0 +1,42 @@
+package in.rahulchaudharyofficial.ds.search;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class SentinalSearch<T> implements Searchable<T> {
+
+    private boolean validate(T item) {
+        boolean isValid = false;
+        for(Method m : item.getClass().getDeclaredMethods()) {
+            if(m.getName()=="equals")
+                isValid = true;
+        }
+        return isValid;
+    }
+
+    @Override
+    public SearchResultModel search(List<T> input, T item) {
+        SearchResultModel<T> result = new SearchResultModel<>();
+
+        if(input != null && !input.isEmpty() && item != null) {
+            if(validate(item))
+            {
+                int index = 0;
+                while(index < input.size() && !item.equals(input.get(index)))
+                {
+                    index++;
+                }
+                if(index < input.size() && item.equals(input.get(index)))
+                {
+                    result.setPosition(index);
+                    result.setModel(input.get(index));
+                    result.setStatus(ResponseStatus.SUCCESS);
+                }
+            }
+            else
+                throw new RuntimeException("Type <T> Must override equals and hashCode");
+        }
+
+        return result;
+    }
+}
