5789a962868d78ce49043e98ff7f05101da0b7ad
rahul chaudhary <rahul.chaudhary.1711@gmail.com>
2022-07-09 23:22:54 +0530

    more search changes added

--- src/main/java/in/rahulchaudharyofficial/ds/search/BinarySearch.java
+++ src/main/java/in/rahulchaudharyofficial/ds/search/BinarySearch.java
@@ -18,16 +18,16 @@ public class BinarySearch<T extends Comparable> implements Searchable<T> {
 
         if(input != null && !input.isEmpty() && item != null) {
             int mid = input.size()/2;
-            if(item.compareTo(input.get(mid)) == 0)
-            {
+            if (item.compareTo(input.get(mid)) < 0) {
+                return search(input.subList(0,mid),item);
+            }
+            else if(item.compareTo(input.get(mid)) > 0){
+                return search(input.subList(mid+1,input.size()),item);
+            }
+            else {
                 result.setPosition(mid);
                 result.setModel(input.get(mid));
                 result.setStatus(ResponseStatus.SUCCESS);
-            } else if (item.compareTo(input.get(mid)) < 0) {
-                return search(input.subList(0,mid),item);
-            }
-            else {
-                return search(input.subList(mid+1,input.size()),item);
             }
         }
         return result;
